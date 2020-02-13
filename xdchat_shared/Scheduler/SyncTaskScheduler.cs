2f4aebe8546691bed68be6b15a65e557037c32da
florian <privat@florian.ws>
2020-02-13 14:31:52 +0100

    Add sync/async scheduler

---
+++ xdchat_shared/Scheduler/SyncTaskScheduler.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XdChatShared.Scheduler {
+    public class SyncTaskScheduler : TaskScheduler, IDisposable {
+        private readonly ConcurrentQueue<Task> tasks = new ConcurrentQueue<Task>();
+
+        private readonly Thread workingThread;
+
+        private readonly AutoResetEvent signalThread = new AutoResetEvent(false);
+
+        private bool stopThread;
+
+        private bool disposed;
+
+        public SyncTaskScheduler(string name) {
+            workingThread = new Thread(RunThread) {Name = name};
+            workingThread.Start();
+        }
+        
+        protected sealed override void QueueTask(Task task) {
+            if (stopThread) {
+                throw new InvalidOperationException("Scheduler can't handle new tasks, because it has been stopped.");
+            }
+
+            tasks.Enqueue(task);
+            signalThread.Set();
+        }
+
+        private void RunThread() {
+            while (true) {
+                if (stopThread && tasks.IsEmpty)
+                    return;
+
+                signalThread.WaitOne();
+
+                while (tasks.TryDequeue(out Task item)) {
+                    this.TryExecuteTask(item);
+                }
+            }
+        }
+
+        protected sealed override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued) {
+            return this.IsMainThread && base.TryExecuteTask(task);
+        }
+        
+        public sealed override int MaximumConcurrencyLevel => 1;
+
+        protected sealed override IEnumerable<Task> GetScheduledTasks() {
+            bool lockTaken = false;
+            try {
+                Monitor.TryEnter(tasks, ref lockTaken);
+                if (lockTaken)
+                    return tasks.ToArray();
+                else
+                    throw new NotSupportedException("Tasks queue can't be locked");
+            }
+            finally {
+                if (lockTaken) Monitor.Exit(tasks);
+            }
+        }
+
+        public int ScheduledTaskCount => tasks.Count;
+
+        public bool IsMainThread => Thread.CurrentThread.ManagedThreadId == workingThread.ManagedThreadId;
+
+        #region IDisposable implementation
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing) {
+            if (disposed)
+                return;
+
+            if (disposing) {
+                stopThread = true;
+                signalThread.Set();
+            }
+
+            disposed = true;
+        }
+
+        #endregion
+    }
+}
---
+++ xdchat_shared/Scheduler/XdScheduler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace XdChatShared.Scheduler {
+    public class XdScheduler {
+        private readonly SyncTaskScheduler mainThreadScheduler = new SyncTaskScheduler("MainThread");
+        private readonly ConcurrentDictionary<int, Thread> asyncThreads = new ConcurrentDictionary<int, Thread>();
+
+        public int QueuedSyncTasks => mainThreadScheduler.ScheduledTaskCount;
+        public int RunningAsyncTasks => asyncThreads.Count;
+
+        public void RunSync(Action action) {
+            Task.Factory.StartNew(action, CancellationToken.None, TaskCreationOptions.None, this.mainThreadScheduler);
+        }
+
+        public void RunAsync(string name, Action action) {
+            Thread thread = new Thread(() => {
+                action.Invoke();
+                asyncThreads.TryRemove(Thread.CurrentThread.ManagedThreadId, out _);
+            });
+            
+            asyncThreads[thread.ManagedThreadId] = thread;
+            thread.Name = $"AsyncThread-{name}-{thread.ManagedThreadId}-";
+            
+            thread.Start();
+        }
+        
+        public void CheckIsMainThread() {
+            if (mainThreadScheduler.IsMainThread) return;
+            throw new InvalidOperationException("Not running on main thread");
+        }
+    }
+}

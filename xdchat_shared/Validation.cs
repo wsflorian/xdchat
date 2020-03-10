e7460bd862983cf6ffbfcef257502aa7bb4a0896
florian <privat@florian.ws>
2020-03-10 14:52:40 +0100

    Add IpPort validation

--- xdchat_shared/Validation.cs
+++ xdchat_shared/Validation.cs
@@ -1,9 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace XdChatShared {
     public static class Validation {
+        private static Regex _ipAddressRegex = new Regex(@"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
+        private static Regex _hostnameRegex = new Regex(@"^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$");
+        
+        public static bool IsValidHost(string input) {
+            return _ipAddressRegex.IsMatch(input) || _hostnameRegex.IsMatch(input);
+        }
+
+        public static bool IsValidPort(string input) {
+            return ushort.TryParse(input, out _);
+        }
+        
+        public static bool IsValidHostPort(string input) {
+            int portIndex = input.IndexOf(':');
+            if (portIndex == -1) {
+                return IsValidHost(input);
+            }
+
+            string host = input.Substring(0, portIndex);
+            string port = input.Substring(portIndex + 1);
+            
+            return IsValidHost(host) && IsValidPort(port);
+        }
+
         public static bool IsValidMessageText(string text) {
             if (string.IsNullOrEmpty(text) || text.Length > 1024) return false;
             return !text.Any(ch => char.IsControl(ch) || char.IsSeparator(ch) && ch != ' ');
